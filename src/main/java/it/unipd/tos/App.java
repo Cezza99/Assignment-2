f63ca26fa17d123927cfb99db640903f98505c6a
Cezza99 <omodeicesare@gmail.com>
2020-12-09 01:59:22 +0100

    Config commit

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [CESARE] [OMODEI] [1187460]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
