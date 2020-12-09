13213b8cfa96951e8f08243aa6c3c69ff5dd9f73
Cezza99 <omodeicesare@gmail.com>
2020-12-09 06:05:46 +0100

    close #6

--- src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
@@ -6,11 +6,12 @@ package it.unipd.tos.business;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
+import java.time.LocalTime;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
+import java.util.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,6 +28,7 @@ public class TakeAwayBillTest {
     @Before  
     public void BeforeClass() {
         tkw_bill = new TakeAwayBillImp();
+        TakeAwayBillImp.ordiniGratis=10;
     }
 
     @Test
@@ -37,7 +39,7 @@ public class TakeAwayBillTest {
         order.add(new MenuItem(ItemType.Bevande,"Coca Cola",2)); 
 
         try {
-            assertEquals(9.5, tkw_bill.getOrderPrice(order, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(9.5, tkw_bill.getOrderPrice(order, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)), LocalTime.of(17,0)),0);
         } catch (RestaurantBillException ex) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -51,7 +53,7 @@ public class TakeAwayBillTest {
         List<MenuItem> empty_order = new ArrayList<MenuItem>();
 
         try {
-            assertEquals(0, tkw_bill.getOrderPrice(empty_order, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(0, tkw_bill.getOrderPrice(empty_order, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException ex) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -71,7 +73,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Budini,"Pinguino",5));
 
         try {
-            assertEquals(24, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(24, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -93,7 +95,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Budini,"Pinguino",5));
 
         try {
-            assertEquals(26, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(26, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -115,7 +117,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Budini,"Pinguino",10));
 
         try {
-            assertEquals(56.7, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(56.7, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -135,7 +137,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Budini,"Pinguino",20));
 
         try {
-            assertEquals(54, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(54, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -151,7 +153,7 @@ public class TakeAwayBillTest {
         for(int i = 0; i < 31; i++) {
             list.add(new MenuItem(ItemType.Budini, "Pinguino", 2));
         }
-        tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)));
+        tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0));
     }
     
     @Test
@@ -163,7 +165,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Bevande,"Coca Cola",2));
 
         try {
-            assertEquals(9.5, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(9.5, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -183,7 +185,7 @@ public class TakeAwayBillTest {
         list.add(new MenuItem(ItemType.Gelati,"Nocciola",1));
 
         try {
-            assertEquals(6, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13))),0);
+            assertEquals(6, tkw_bill.getOrderPrice(list, new User("000001","Cesare","Omodei",LocalDate.of(1999,4,13)),LocalTime.of(17,0)),0);
         } catch (RestaurantBillException e) {
             // TODO Auto-generated catch block
             fail("Test failed");
@@ -191,9 +193,130 @@ public class TakeAwayBillTest {
         }
 
     }
+    
+    @Test
+    public void testOrdineGratis() {
+        List<MenuItem> list = new ArrayList<MenuItem>();
+        list.add(new MenuItem(ItemType.Gelati,"Cioccolato",10));
+        Random seed= new Random(1000);
+        double sum=0;
+
+        for(int i=0;i<50;i++) {
+            try {
+                sum+= tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.now()), LocalTime.of(18, 15));
+            } catch (RestaurantBillException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+
+        Vector<Integer> test = new Vector<Integer>(100);
 
 
 
+
+        for (int i=0;i<100;++i) {
+            int x=seed.nextInt() & Integer.MAX_VALUE;
+            test.add(x%100);
+        }
+
+        double tot=0;
+        int gratis=0;
+        for (int i=0; i<50;++i) {
+
+            if(test.elementAt(i)<50 && gratis<10) {     
+                gratis++;
+                tot+=0;
+            } else tot+=10;
+
+        }
+
+        assertEquals(tot,sum,0);
+
+
+    }
+
+
+    @Test
+    public void testOrdineGratis_LimiteMassimo() {
+        List<MenuItem> list = new ArrayList<MenuItem>();
+        list.add(new MenuItem(ItemType.Gelati,"Cioccolato",10));
+        double sum=0;
+
+        for(int i=0;i<50;i++) {
+            try {
+                sum+= tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.now()), LocalTime.of(19,00,01));
+            } catch (RestaurantBillException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        assertEquals(500,sum,0);
+
+
+    }
+
+    @Test
+    public void testOrdiniGratis_LimiteMinimo() {
+        List<MenuItem> list = new ArrayList<MenuItem>();
+        list.add(new MenuItem(ItemType.Gelati,"Cioccolato",10));       
+        double sum=0;
+
+        for(int i=0;i<50;i++) {
+            try {
+                sum+= tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.now()), LocalTime.of(17,59,59));
+            } catch (RestaurantBillException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        assertEquals(500,sum,0);
+
+
+    }
+
+    @Test
+    public void testOrdiniGratisLimiteMComp() {
+        List<MenuItem> list = new ArrayList<MenuItem>();
+        list.add(new MenuItem(ItemType.Gelati,"Cioccolato",10));       
+        double sum=0;
+
+        for(int i=0;i<50;i++) {
+            try {
+                sum+= tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.now()), LocalTime.of(18,00,00));
+            } catch (RestaurantBillException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        assertEquals(400,sum,0);
+
+
+    }
+
+    @Test
+    public void testOrdiniGratisLimiteSComp() {
+        List<MenuItem> list = new ArrayList<MenuItem>();
+        list.add(new MenuItem(ItemType.Gelati,"Cioccolato",10));       
+        double sum=0;
+
+        for(int i=0;i<50;i++) {
+            try {
+                sum+= tkw_bill.getOrderPrice(list,new User("000001","Cesare","Omodei",LocalDate.now()), LocalTime.of(19,00,00));
+            } catch (RestaurantBillException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        assertEquals(400,sum,0);
+
+
+    }
 } 
 
     
